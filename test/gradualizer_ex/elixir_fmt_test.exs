4b1ab295afbadf5a143fa0c9c89954b6175bdb8f
Przemysław Wojtasik <przemyslaw.wojtasik@erlang-solutions.com>
2021-10-12 22:47:52 +0200

    Init tests

--- test/gradualizer_ex/elixir_fmt_test.exs
+++ test/gradualizer_ex/elixir_fmt_test.exs
@@ -3,6 +3,9 @@ defmodule GradualizerEx.ElixirFmtTest do
   doctest GradualizerEx.ElixirFmt
 
   alias GradualizerEx.ElixirFmt
+  alias GradualizerEx.TestHelpers
+  alias GradualizerEx.AstSpecifier
+  import GradualizerEx.TestHelpers
 
   @example_module_path "test/examples/simple_app.ex"
 
@@ -16,6 +19,28 @@ defmodule GradualizerEx.ElixirFmtTest do
       {:ok, "29   def bool_id(x) do\n30     _x = 13\n\e[4m\e[31m31     12\e[0m\n32   end\n33 "}
 
     assert res == expected
+  end
+
+  describe "types format" do
+    test "wrong return type" do
+      {_tokens, ast} = load("/type/Elixir.WrongRet.beam", "/type/wrong_ret.ex")
+      opts = []
+      errors = type_check_file(ast, opts)
+      for e <- errors do
+        :io.put_chars(e)
+      end
+
+    end
+  end
+
+  def type_check_file(ast, opts) do
+    forms = AstSpecifier.specify(ast)
+    opts = Keyword.put(opts, :return_errors, true)
+    opts = Keyword.put(opts, :forms, forms)
+
+    forms
+    |> :gradualizer.type_check_forms(opts)
+    |> Enum.map(fn {_, err} -> ElixirFmt.format_error(err, opts) end)
   end
 
   @tag :skip
